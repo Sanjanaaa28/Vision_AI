b0045b622dfb1e0392f14f2d27cef6aca53e7e0b
Sanjanaaa28 <sanjanakush1028@gmail.com>
2026-07-11 01:44:09 +0530

    Add Python basics notebooks

---
+++ PythonBasic/basic.py
@@ -0,0 +1,22 @@
+# variables 
+a=10 
+print(a)
+print(type(a))
+
+a=10.5 # float variable 
+print(a)
+a="Hello" # string variable ' ' "" ''' '''
+a = True #  Boolean variable 
+a = 1+1j   # Complex variable
+a = 1,2,3,4   # tuple variable
+a = [1,2,3,4]   # list variable ()tuple
+a = {1,2,3,4}  # set variable{}set 
+a = {1: "one", 2:"two", 3: "three"}    # dictionary 
+
+class Myclass:
+    pass
+
+
+# 1_data = 100   # variable starting with number is not allowed 
+# _data = 100 # variable starting with underscore is allowed 
+# @data = 100 # variable starting with @ is not allowed 
